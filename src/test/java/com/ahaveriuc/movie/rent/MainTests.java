6fd6c407d3597a559cc392dad5cc82e1045036d4
Andrei HAVERIUC <haveriuc.a@gmail.com>
2025-01-20 12:52:46 +0200

    Initial commit

---
+++ src/test/java/com/ahaveriuc/movie/rent/MainTests.java
@@ -0,0 +1,13 @@
+package com.ahaveriuc.movie.rent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
